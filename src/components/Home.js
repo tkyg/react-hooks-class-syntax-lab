c2cd345f1af6fa02bbfedbd12dc232fe8eacf4b4
tkyg <80114433+tkyg@users.noreply.github.com>
2022-07-22 16:27:19 -0400

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,27 @@
 import React from "react";
 
-function Home(props) {
-  return (
-    <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.username} is a Web Developer from {props.city}
+// function Home(props) {
+//   return (
+//     <div id="home">
+//       <h1 style={{ color: props.color }}>
+//         {props.username} is a Web Developer from {props.city}
+//       </h1>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+class Home extends React.Component {
+  render() {
+    return(
+      <div id="home">
+      <h1 style={{ color: this.props.color }}>
+        {this.props.username} is a Web Developer from {this.props.city}
       </h1>
     </div>
-  );
+    )
+  }
 }
 
-export default Home;
+export default Home
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-function NavBar() {
-  return (
+// function NavBar() {
+//   return (
+//     <nav>
+//       <a href="#home">Home</a>
+//       <a href="#about">About</a>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
+
+class NavBar extends React.Component {
+  render() {
+    return (
     <nav>
       <a href="#home">Home</a>
       <a href="#about">About</a>
     </nav>
-  );
+    )
+  }
 }
 
-export default NavBar;
+export default NavBar
